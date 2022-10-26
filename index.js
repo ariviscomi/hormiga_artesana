33aa9d1164253747bd77b21909e280c3fccdc035
ariviscomi <arielviscomi.mail@gmail.com>
2022-10-26 18:08:03 -0300

    (26/10/22) Menu hamburguesa

--- index.js
+++ index.js
@@ -1,158 +1,165 @@
-//* ========================= Menu hamburguesa: ========================= *//
+//TODO ====================================== Menu Hamburguesa ====================================== TODO//
 
-let menu = document.querySelector('.menu_icon');
+const toggleButton = document.getElementById("button_menu");
+const navWrapper = document.getElementById("nav");
 
-menu.addEventListener('click', ()=>{
-    menu.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+	toggleButton.classList.toggle("close");
+	navWrapper.classList.toggle("show");
+});
 
-//! ============================================= Funciones ============================================= !//
+navWrapper.addEventListener("click", e => {
+	if (e.target.id === "nav") {
+		navWrapper.classList.remove("show");
+		toggleButton.classList.remove("close");
+	}
+});
 
-const vectorCero = (vector,initial) => {
+//TODO ====================================== Carrito de Compra ====================================== TODO//
+/*
+let continuar = true;                          	//Condicion de salida del ciclo
+let carrito = 0, prod, decision;               	//Variables enteras
+let vProductos = [1000, 1500, 2000, 1600];     	//Vector de precios de productos
+let vStock = [5, 2, 0, 7];							//Stock de productos
+let vContador = [];                            	//Contador de productos agregados al carrito
 
-    for (let i = 0; i < initial; i++) {
-    
-        vector[i] = 0;
-    }
-    
-    let j;
-    
-    for (let i = 0; i < initial; i++) {
-    
-        j = i + 1;
-        console.log("elemento " + j + ": " + vector[i]);
-    }
+vectorCero(vContador, 4);                       	//Inicia en cero el contenido del vector
+console.log("cantidad comprada:", vContador);
+
+const vectorCero = (vector, initial) => {
+
+	for (let i = 0; i < initial; i++) {
+
+		vector[i] = 0;
+	}
+
+	let j;
+
+	for (let i = 0; i < initial; i++) {
+
+		j = i + 1;
+		console.log("elemento " + j + ": " + vector[i]);
+	}
 }
 
-const iniciarSesion = () =>{
-    let nombre, salida;
-    
-    alert("Para continuar inicie sesion!");
-    
-    do{
-        nombre= prompt("Ingrese su nombre:");
+const iniciarSesion = () => {
+	let nombre, salida;
 
-        if((nombre != "") && ((nombre.toLowerCase()==="ariel") || (nombre.toLowerCase()==="damian") || (nombre.toLowerCase()==="florencia") || (nombre.toLowerCase()==="valentina"))){
-    
-            alert("Bienvenid@" + nombre + "!!");
-            salida=false;
-        }
-        else if(nombre == ""){
-            alert("No ingresaste ningun nombre!");
-            salida=true;            
-        }
-        else{
-            alert("No existe un usuario con ese nombre!");
-            salida=true;
-        }
-    
-    }while(salida)
+	alert("Para continuar inicie sesion!");
 
-    return nombre
+	do {
+		nombre = prompt("Ingrese su nombre:");
+
+		if ((nombre != "") && ((nombre.toLowerCase() === "ariel") || (nombre.toLowerCase() === "damian") || (nombre.toLowerCase() === "florencia") || (nombre.toLowerCase() === "valentina"))) {
+
+			alert("Bienvenid@" + nombre + "!!");
+			salida = false;
+		}
+		else if (nombre == "") {
+			alert("No ingresaste ningun nombre!");
+			salida = true;
+		}
+		else {
+			alert("No existe un usuario con ese nombre!");
+			salida = true;
+		}
+
+	} while (salida)
+
+	return nombre
 }
 
-const validarProd = (prod,stock,indice) => {
+const validarProd = (prod, stock, indice) => {
 
-    if((prod>0 && prod<=indice) && (stock[prod-1] > 0) ){
-        return true;
-    }
-    else{
-        return false;
-    }
+	if ((prod > 0 && prod <= indice) && (stock[prod - 1] > 0)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 
 }
 
 const listarProd = (vector) => {
 
-    let lista = "Su Compra fue de:\n";
-    
-    if (vector[0] > 0) {
+	let lista = "Su Compra fue de:\n";
 
-        lista = lista + "\n    Mate diseño montaña: x" + vector[0];
-    }
-    
-    if (vector[1] > 0) {
-    
-        lista = lista + "\n    Mate diseño MapaMundi: x" + vector[1];
-    }
-    
-    if (vector[2] > 0) {
-    
-        lista = lista + "\n    Mate equipo de futbol: x" + vector[2];
-    }
-    
-    if (vector[3] > 0) {
-    
-        lista = lista + "\n    Mate Mandala: x" + vector[3];
-    }    
+	if (vector[0] > 0) {
 
-    return lista;
+		lista = lista + "\n    Mate diseño montaña: x" + vector[0];
+	}
+
+	if (vector[1] > 0) {
+
+		lista = lista + "\n    Mate diseño MapaMundi: x" + vector[1];
+	}
+
+	if (vector[2] > 0) {
+
+		lista = lista + "\n    Mate equipo de futbol: x" + vector[2];
+	}
+
+	if (vector[3] > 0) {
+
+		lista = lista + "\n    Mate Mandala: x" + vector[3];
+	}
+
+	return lista;
 }
-
-//? ============================================= Variables ============================================= ?//
-
-let continuar = true;                          	//Condicion de salida del ciclo
-let carrito = 0, prod, decision;               	//Variables enteras
-let vProductos = [1000, 1500, 2000, 1600];     	//Vector de precios de productos
-let vStock = [5,2,0,7];							//Stock de productos
-let vContador = [];                            	//Contador de productos agregados al carrito
-
-vectorCero(vContador,4);                       	//Inicia en cero el contenido del vector
-console.log("cantidad comprada:",vContador);
-
-//* ========================================= Carrito de Compra ========================================= *//
 
 const nombre = iniciarSesion();
 
 while (continuar === true) {
 
-    do {
-        prod = parseInt(prompt(`Seleccione un producto para agregar al carrito:\n    1.Mate diseño montaña\n    2.Mate diseño MapaMundi\n    3.Mate equipo de futbol\n    4.Mate Mandala`));
+	do {
+		prod = parseInt(prompt(`Seleccione un producto para agregar al carrito:\n    1.Mate diseño montaña\n    2.Mate diseño MapaMundi\n    3.Mate equipo de futbol\n    4.Mate Mandala`));
 
-        if (validarProd(prod,vStock,4)) {
+		if (validarProd(prod, vStock, 4)) {
 
-            carrito += vProductos[prod - 1];
+			carrito += vProductos[prod - 1];
 			vContador[prod - 1] = vContador[prod - 1] + 1;
-			vStock[prod-1] = vStock[prod-1] - 1;
+			vStock[prod - 1] = vStock[prod - 1] - 1;
 
-			console.log("cantidad comprada:",vContador);
-			console.log("stock:",vStock);
-        }
-        else {
-            alert("No tenemos ese producto!");
+			console.log("cantidad comprada:", vContador);
+			console.log("stock:", vStock);
+		}
+		else {
+			alert("No tenemos ese producto!");
 
 			do {
 				decision = parseInt(prompt(nombre + ", Desea seguir comprando?\n    1=Si;\n    2=No"))
-		
+
 				if (decision != 2 && decision != 1) {
 					alert("No entendi tu respuesta!")
 				}
-		
+
 			} while (decision != 2 && decision != 1);
 
-			if(decision === 2){
+			if (decision === 2) {
 				alert("Gracias!, Vuelvas prontos ;D");
 				break;
 			}
-        }
+		}
 
-    } while (!validarProd(prod,vStock,4));
+	} while (!validarProd(prod, vStock, 4));
 
 
-    alert(nombre + ", \n    Su Carrito vale: $" + carrito);
+	alert(nombre + ", \n    Su Carrito vale: $" + carrito);
 
-    do {
-        decision = parseInt(prompt(nombre + ", Desea seguir comprando?\n    1=Si;\n    2=No"))
+	do {
+		decision = parseInt(prompt(nombre + ", Desea seguir comprando?\n    1=Si;\n    2=No"))
 
-        if (decision != 2 && decision != 1) {
-            alert("No entendi tu respuesta!")
-        }
+		if (decision != 2 && decision != 1) {
+			alert("No entendi tu respuesta!")
+		}
 
-    } while (decision != 2 && decision != 1);
+	} while (decision != 2 && decision != 1);
 
-    if (decision === 2) {
-        continuar = false;
-    }
+	if (decision === 2) {
+		continuar = false;
+	}
 }
 
 alert(nombre + ",\nGracias por su compra!\n" + listarProd(vContador) + "\n\nEl Total fue de: $" + carrito);
+
+*/
